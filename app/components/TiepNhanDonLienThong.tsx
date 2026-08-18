b8afa8cfac988988804fbb91e9f9e21c0ed24c42
pinkiepetites <buiphuongthaout@gmail.com>
2026-08-18 13:58:06 +0700

    fix: change iframe source to server public path to fix display issue

--- app/components/TiepNhanDonLienThong.tsx
+++ app/components/TiepNhanDonLienThong.tsx
@@ -4,7 +4,7 @@ export default function TiepNhanDonLienThong() {
   return (
     <div style={{ width: '100%', height: '90vh', border: 'none', margin: 0, padding: 0, overflow: 'hidden' }}>
       <iframe
-        src="file:///c:/Users/Gtel-Ict/Downloads/Prototype_TiepNhanDonLienThong%20(1).html"
+        src="/Prototype_TiepNhanDonLienThong.html"
         title="Tiếp nhận đơn liên thông"
         style={{ width: '100%', height: '100%', border: 'none' }}
       />
